55a03a51fc0dac94bf14c711c902783255baf1cc
Eduarda Vieira <159597766+eduardavieira-dev@users.noreply.github.com>
2024-11-08 11:43:10 -0300

    add matrizes

---
+++ Estudos/matrizes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Solicitar o tamanho da matriz
+    printf("Digite o tamanho da matriz quadrada (n x n): ");
+    scanf("%d", &n);
+
+    int matriz[n][n];
+
+    // Preencher a matriz com valores fornecidos pelo usuário
+    printf("Digite os elementos da matriz:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("Elemento [%d][%d]: ", i+1, j+1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    // Imprimir os elementos da diagonal principal
+    printf("\nElementos da diagonal principal:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", matriz[i][i]);
+    }
+    printf("\n");
+
+    // Imprimir os elementos da diagonal secundária
+    printf("\nElementos da diagonal secundária:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", matriz[i][n-1-i]);
+    }
+    printf("\n");
+
+    // Imprimir a matriz completa
+    printf("\nMatriz completa:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
